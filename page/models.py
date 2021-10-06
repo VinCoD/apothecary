e951643bfb9dbd2cb80b176ea0cc6fd0a81628ea
VinCoD <5895.2020@students.ku.ac.ke>
2021-10-06 06:34:09 +0300

    Added Parents and Children tables.

--- page/models.py
+++ page/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Parent(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    age = models.IntegerField
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Child(models.Model):
+    parent = models.ForeignKey(Parent, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Children'
+
+    def __str__(self):
+        return self.name
