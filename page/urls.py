8e25bb0e6f249946dd05db32b8bdc2350711cdb8
VinCoD <odipovincent161@gmail.com>
2021-11-21 23:25:31 +0300

    Add parent page was added

--- page/urls.py
+++ page/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('available_meds/', views.available_meds, name="available_meds"),
     path('parents/', views.parents, name='parents'),
     path('parents/<int:ida>/', views.children, name="children"),
+    path('new_parent/', views.new_parent, name='new_parent'),
+    path('new_child/', views.new_child, name='new_child'),
 ]
--- page/views.py
+++ page/views.py
@@ -1,20 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from page.forms import ParentForm
 from .models import Parent
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'page/index.html')
 
 def available_meds(request):
-    return render(request, 'available_meds.html')
+    return render(request, 'page/available_meds.html')
 
 def parents(request):
     parents = Parent.objects.all()
     context = {'parents': parents}
-    return render(request, 'parents.html', context)
+    return render(request, 'page/parents.html', context)
 
 def children(request, ida):
     parent = Parent.objects.get(id=ida)
     children = parent.child_set.order_by('-date_added')
     context = {'children': children, 'parent':parent}
-    return render(request, 'children.html', context)
+    return render(request, 'page/children.html', context)
+
+def new_parent(request):
+    if request.method != 'POST':
+        form = ParentForm()
+    else:
+        form = ParentForm(data=request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect('page:parents')
+    
+    context = {'form': form}
+    return render(request, 'page/new_parent.html', context)
+    
